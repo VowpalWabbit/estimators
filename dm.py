ccf877857ca1ffe8eb0e89b28b9cc84ba2b28c13
americast <sayan.sinha@iitkgp.ac.in>
2020-03-11 11:04:05 +0530

    Shift to vw

--- dm.py
+++ dm.py
@@ -1,7 +1,5 @@
-import math
-from sklearn.linear_model import LinearRegression
-import numpy as np
-import pudb
+from vowpalwabbit import pyvw
+
 
 
 class Estimator:
@@ -15,19 +13,29 @@ class Estimator:
         #################################################################################################
 
         self.data = {'pred': [], 'r': [], 'N': 0}
+        self.format = []
+        self.test = ["| r: 0.", "| r: 1."]
+
 
     def add_example(self, p_log, r, p_pred, count=1):
         self.data['N'] += count
         if count > 1:
             self.data['pred'].extend(p_pred)
             self.data['r'].extend(r)
+            for i, p in enumerate(p_pred):
+                self.format.append(str(p)+" | r: "+str(float(r[i])))
         elif count == 1:
             self.data['pred'].append(p_pred)
             self.data['r'].append(r)
+            self.format.append(str(p_pred)+" | r: "+str(float(r)))
         
     def get_estimate(self):
         if self.data['N'] == 0:
             raise('Error: No data point added')
 
-        reg = LinearRegression().fit(np.array(self.data['r']).reshape(-1, 1), np.array(self.data['pred']).reshape(-1, 1))
-        return reg.coef_[0][0], reg.intercept_[0]
+        model = pyvw.vw(quiet=True)
+
+        for example in self.format:
+            model.learn(example)
+
+        return model.predict(self.test[0]), model.predict(self.test[1])
