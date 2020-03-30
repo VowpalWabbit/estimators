c15a8f0e4baf1f3eb4c36b522f22386cfb2b737a
Jack Gerrits <jackgerrits95@gmail.com>
2020-03-30 18:22:13 -0400

    Use close comparison rather than equality for floats

--- test/test_pi.py
+++ test/test_pi.py
@@ -8,12 +8,13 @@ def test_single_slot_pi_equivalent_to_ips():
     """PI should be equivalent to IPS when there is only a single slot"""
     pi_estimator = pseudo_inverse.Estimator()
     ips_estimator = ips_snips.Estimator()
+    is_close = lambda a, b: abs(a - b) <= 1e-6 * (1 + abs(a) + abs(b))
 
     p_logs = [0.8, 0.25, 0.5, 0.2]
     p_preds = [0.6, 0.4, 0.3, 0.9]
-    rs = [0.1, 0.2, 0, 1.0]
+    rewards = [0.1, 0.2, 0, 1.0]
 
-    for p_log, r, p_pred in zip(p_logs, rs, p_preds):
+    for p_log, r, p_pred in zip(p_logs, rewards, p_preds):
         pi_estimator.add_example([p_log], r, [p_pred])
         ips_estimator.add_example(p_log, r, p_pred)
-        assert pi_estimator.get_estimate('pi') == ips_estimator.get_estimate('ips')
+        assert is_close(pi_estimator.get_estimate('pi') , ips_estimator.get_estimate('ips'))
