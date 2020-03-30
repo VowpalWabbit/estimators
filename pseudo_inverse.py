4abd4faab1aab7b65e5736270cf31d74b873bc96
Jack Gerrits <jackgerrits95@gmail.com>
2020-03-30 17:38:33 -0400

    Address comments on PR

--- pseudo_inverse.py
+++ pseudo_inverse.py
@@ -1,15 +1,21 @@
 import math
 from scipy.stats import beta
 
-# PseudoInverse estimator for slate recommendation
-# https://arxiv.org/abs/1605.04812\
+# PseudoInverse estimator for slate recommendation. The following implements the
+# case for a Cartesian product when mu is a product distribution. This can be
+# seen in example 4 of the paper.
+# https://arxiv.org/abs/1605.04812
 
 class Estimator:
     def __init__(self):
         self.data = {'n':0.,'N':0, 'SoS':0}
 
     def add_example(self, p_logs, r, p_preds, count=1):
-        """Expects lists for logged probabilities and predicted probabilities. These should correspond to each slot."""
+        """Expects lists for logged probabilities and predicted probabilities. These should correspond to each slot.
+        This function is implemented under the simplifying assumptions of
+        example 4 in the paper 'Off-policy evaluation for slate recommendation'
+        where the slate space is a cartesian product and the logging policy is a
+        product distribution"""
         if not isinstance(p_logs, list) or not isinstance(p_preds, list):
             raise('Error: p_logs and p_preds must be lists')
 
@@ -21,7 +27,7 @@ class Estimator:
         num_slots = len(p_logs)
         for p_log, p_pred in zip(p_logs, p_preds):
             p_over_ps += p_pred/p_log
-        p_over_ps -= num_slots + 1
+        p_over_ps -= num_slots - 1
 
         if r != 0:
             self.data['n'] += r*p_over_ps*count
