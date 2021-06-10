972e05484cdb32319adaa2be31791692f5004b8b
JuiP <juipradhan2k@gmail.com>
2021-06-10 18:32:35 +0530

    Split pseudo_inverse into 2 classes: Estimator and Interval

--- estimators/slates/pseudo_inverse.py
+++ estimators/slates/pseudo_inverse.py
@@ -8,6 +8,42 @@ from scipy.stats import beta
 
 class Estimator:
     def __init__(self):
+        self.data = {'n':0.,'N':0}
+
+    def add_example(self, p_logs, r, p_preds, count=1):
+        """Expects lists for logged probabilities and predicted probabilities. These should correspond to each slot.
+        This function is implemented under the simplifying assumptions of
+        example 4 in the paper 'Off-policy evaluation for slate recommendation'
+        where the slate space is a cartesian product and the logging policy is a
+        product distribution"""
+        if not isinstance(p_logs, list) or not isinstance(p_preds, list):
+            raise('Error: p_logs and p_preds must be lists')
+
+        if(len(p_logs) != len(p_preds)):
+            raise('Error: p_logs and p_preds must be the same length, found {} and {} respectively'.format(len(p_logs), len(p_preds)))
+
+        self.data['N'] += count
+        p_over_ps = 0
+        num_slots = len(p_logs)
+        for p_log, p_pred in zip(p_logs, p_preds):
+            p_over_ps += p_pred/p_log
+        p_over_ps -= num_slots - 1
+
+        if r != 0:
+            self.data['n'] += r*p_over_ps*count
+
+    def get_estimate(self, type):
+        if self.data['N'] == 0:
+            raise('Error: No data point added')
+
+        if type == 'pi':
+            return self.data['n']/self.data['N']
+        else:
+            raise('Error: Incorrect estimator type {}. Supported options are pi'.format(type))
+
+
+class Interval:
+    def __init__(self):
         self.data = {'n':0.,'N':0, 'SoS':0}
 
     def add_example(self, p_logs, r, p_preds, count=1):
@@ -32,15 +68,6 @@ class Estimator:
         if r != 0:
             self.data['n'] += r*p_over_ps*count
             self.data['SoS'] += ((r*p_over_ps)**2)*count
-
-    def get_estimate(self, type):
-        if self.data['N'] == 0:
-            raise('Error: No data point added')
-
-        if type == 'pi':
-            return self.data['n']/self.data['N']
-        else:
-            raise('Error: Incorrect estimator type {}. Supported options are pi'.format(type))
 
     def get_interval(self, type, alpha=0.05):
         bounds = []
