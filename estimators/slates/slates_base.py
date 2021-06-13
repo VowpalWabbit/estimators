53c9b7ae1dc16b9c727c2fafe655160654c7ce10
JuiP <juipradhan2k@gmail.com>
2021-06-13 21:39:32 +0530

    Changes for slates
    - Renamed file from slates_helper to slates_base
    - Added gaussian.py
    - Removed type from get_interval
    - Removed type from get_estimate
    - Change doc strings for the slates interface
    - Changed class names
    - Changed data type of count
    - Fixed data type of p_log and p_pred
    - Removed unused imports

---
+++ estimators/slates/slates_base.py
@@ -0,0 +1,50 @@
+""" Interface for implementation of slates estimator """
+
+from abc import ABC, abstractmethod
+from typing import List
+
+class Estimator(ABC):
+	""" Interface for implementation of slates estimator """
+
+	@abstractmethod
+	def add_example(self, p_log: List, r: float, p_pred: List, count: float) -> None:
+		""" 
+		Args:
+			p_log: probability of the logging policy
+			r: reward for choosing an action in the given context
+			p_pred: predicted probability of making decision
+			count: weight
+		"""
+		...
+
+	@abstractmethod
+	def get_estimate(self) -> float:
+		""" Calculates the selected estimator
+		Returns:
+			The estimator value
+		"""
+		...
+
+class Interval(ABC):
+	""" Interface for implementation of slates estimator interval """
+
+	@abstractmethod
+	def add_example(self, p_log: List, r: float, p_pred: List, count: float) -> None:
+		""" 
+		Args:
+			p_log: probability of the logging policy
+			r: reward for choosing an action in the given context
+			p_pred: predicted probability of making decision
+			count: weight
+		"""
+		...
+
+	@abstractmethod
+	def get_interval(self, alpha: float) -> List:
+		""" Calculates the CI
+		Args:
+			alpha: alpha value
+		Returns:
+			Returns the confidence interval as list[float]
+		"""
+		...
