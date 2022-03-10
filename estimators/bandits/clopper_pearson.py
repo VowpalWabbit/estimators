7fa544cc588131846a00259cff1186f3a7235567
Alexey Taymanov <ataymano@gmail.com>
2022-03-10 12:12:18 -0500

    fixes

--- estimators/bandits/clopper_pearson.py
+++ estimators/bandits/clopper_pearson.py
@@ -1,3 +1,4 @@
+from sre_constants import AT_LOCALE
 from scipy.stats import beta
 from estimators.bandits import base
 from typing import List, Optional
@@ -12,19 +13,30 @@ class Interval(base.Interval):
         self.examples_count = 0
         self.weighted_reward = 0
         self.max_weighted_reward = 0
+        self.rmin = rmin
+        self.rmax = rmax
+
+    def _scale(self, r):
+        assert r >= self.rmin and r <= self.rmax, f'Error: {r} is out of [{self.rmin}, {self.rmax}]'
+        return (r - self.rmin) / (self.rmax - self.rmin)
+
+    def _scale_back(self, r):
+        return self.rmin + r * (self.rmax - self.rmin)
 
     def add_example(self, p_log: float, r: float, p_pred: float, count: float = 1.0) -> None:
+        r = self._scale(r)
         self.examples_count += count
         w = p_pred / p_log
         self.weighted_reward += r * w * count
         self.max_weighted_reward = max(self.max_weighted_reward, r * w)
 
     def get(self, alpha: float = 0.05) -> List[Optional[float]]:
+        atol = 1e-10
         if self.max_weighted_reward > 0.0:
             successes = self.weighted_reward / self.max_weighted_reward
             n = self.examples_count / self.max_weighted_reward
-            return [beta.ppf(alpha / 2, successes, n - successes + 1),
-                    beta.ppf(1 - alpha / 2, successes + 1, n - successes)]
+            return [self._scale_back(beta.ppf(alpha / 2, successes, max(n - successes + 1, atol))),
+                    self._scale_back(beta.ppf(1 - alpha / 2, successes + 1, max(n - successes, atol)))]
         return [None, None]
 
     def __add__(self, other: 'Interval') -> 'Interval':
